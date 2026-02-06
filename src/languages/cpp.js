937c8d756c166193891baa9444f6fed28b325f61
hitesh choudhary <hitesh@hiteshchoudhary.com>
2026-02-06 17:38:55 +0530

    add min_memory 512MB for C++ to handle nlohmann/json.hpp compilation

--- src/languages/cpp.js
+++ src/languages/cpp.js
@@ -6,4 +6,6 @@ export default {
   compile_cmd: 'g++ -O2 -std=c++17 -I/usr/local/include -o main *.cpp',
   run_cmd: './main',
   image: 'codebox/gcc:9',
+  // nlohmann/json.hpp needs ~512MB to compile
+  min_memory: 512000,
 };
