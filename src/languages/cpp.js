6012a1fcd6145cc42d2dda003e39a4d3ccce24d6
hitesh choudhary <hitesh@hiteshchoudhary.com>
2026-02-06 17:35:46 +0530

    add nlohmann/json.hpp for C++ and gson.jar for Java with classpath config

--- src/languages/cpp.js
+++ src/languages/cpp.js
@@ -3,7 +3,7 @@ export default {
   name: 'C++ (GCC 9.4.0)',
   is_archived: false,
   source_file: 'main.cpp',
-  compile_cmd: 'g++ -O2 -std=c++17 -o main *.cpp',
+  compile_cmd: 'g++ -O2 -std=c++17 -I/usr/local/include -o main *.cpp',
   run_cmd: './main',
   image: 'codebox/gcc:9',
 };
--- src/languages/java.js
+++ src/languages/java.js
@@ -3,7 +3,7 @@ export default {
   name: 'Java (OpenJDK 17)',
   is_archived: false,
   source_file: 'Main.java',
-  compile_cmd: 'javac *.java',
-  run_cmd: 'java Main',
+  compile_cmd: 'javac -cp .:/usr/local/lib/java/* *.java',
+  run_cmd: 'java -cp .:/usr/local/lib/java/* Main',
   image: 'codebox/java:17',
 };
