efebc03cb30edc758715cbdc045a6fc1dba22db5
hitesh choudhary <hitesh@hiteshchoudhary.com>
2026-02-06 20:08:50 +0530

    use tsconfig with types:[] to ignore incompatible @types/node from additional_files

--- src/languages/typescript.js
+++ src/languages/typescript.js
@@ -3,7 +3,7 @@ export default {
   name: 'TypeScript (5.0.3)',
   is_archived: false,
   source_file: 'ts-main.ts',
-  compile_cmd: 'tsc ts-main.ts --outDir . --esModuleInterop --skipLibCheck',
+  compile_cmd: 'echo \'{"compilerOptions":{"outDir":".","esModuleInterop":true,"skipLibCheck":true,"types":[]},"files":["ts-main.ts"]}\' > tsconfig.json && tsc -p tsconfig.json',
   run_cmd: 'node ts-main.js',
   image: 'codebox/typescript:5',
   // tsc needs ~400MB to load the type system; container must allow this during compilation
